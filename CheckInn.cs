6ea2cfbcbe0951cdb4dc71087dc95eb52d385ee9
rotoyouoi <rotoyouoi@yandex.ru>
2023-10-24 14:53:26 +0500

    Добавьте файлы проекта.

---
+++ CheckInn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InnLibrary
+{
+    public class CheckInn
+    {
+        public bool CorrectFillINN(string innCode)
+        {
+            int[] cof1 = { 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 ,0,0};
+            int[] cof2 = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8,0 };
+            int summ = 0;
+            int summ2 = 0;
+            double ost = 0;
+            double ost2 = 0;
+            if (string.IsNullOrEmpty(innCode))
+            {
+                return false;
+            }
+            if (innCode.Length < 12 || innCode.Length > 12)
+            {
+                return false;
+            }
+            for (int i = 0; i < cof1.Length; i++)
+            {
+                summ = summ + Convert.ToInt32(char.GetNumericValue(innCode[i])) * cof1[i];
+                summ2 = summ2 + Convert.ToInt32(char.GetNumericValue(innCode[i])) * cof2[i];
+            }
+            ost = summ % 11;
+            ost2 = summ2 % 11;
+
+            if (ost > 9)
+            {
+                ost = ost % 10;
+
+            }
+            if (ost2 > 9)
+            {
+                ost2 = ost2 % 10;
+
+            }
+            if (ost == Convert.ToInt32(char.GetNumericValue(innCode[10])) && ost2 == Convert.ToInt32(char.GetNumericValue(innCode[11])))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string innCode = "663555067806 ";
+            int[] cof1 = { 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+            int[] cof2 = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+            int summ = 0;
+            int summ2 = 0;
+            double ost = 0;
+            double ost2 = 0;
+            for (int i = 0; i < cof1.Length; i++)
+            {
+                summ = summ + Convert.ToInt32(innCode[i].ToString()) * cof1[i];
+                summ2 = summ2 + Convert.ToInt32(innCode[i].ToString()) * cof2[i];
+            }
+            ost = summ % 11;
+            ost2 = summ2 % 11;
+
+            if (ost >= 9)
+            {
+                ost = summ % 10;
+
+            }
+            if (ost2 >= 9)
+            {
+                ost2 = summ2 % 10;
+
+            }
+            if (ost == Convert.ToInt32(innCode[10].ToString()) && ost2 == Convert.ToInt32(innCode[11].ToString()))
+            {
+                Console.WriteLine(ost);
+            }
+            
+        }
+    }
+}
